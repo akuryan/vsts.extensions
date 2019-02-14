4ba3513d4d43d7a870a97f58ab35654b27dabcdc
Anton Kuryan <anton.kuryan@colours.nl>
2019-02-14 13:10:17 +0300

    BudgetSaver shall import latest AzureRM function

--- AzureCostsSaver/buildtask/ps_modules/VstsAzureHelpers_/ImportFunctions.ps1
+++ AzureCostsSaver/buildtask/ps_modules/VstsAzureHelpers_/ImportFunctions.ps1
@@ -1,10 +1,47 @@
-﻿function Import-AzureModule {
+﻿function Get-LatestModule {
+    [CmdletBinding()]
+    param([string] $patternToMatch,
+          [string] $patternToExtract)
+    
+    $resultFolder = ""
+    $regexToMatch = New-Object -TypeName System.Text.RegularExpressions.Regex -ArgumentList $patternToMatch
+    $regexToExtract = New-Object -TypeName System.Text.RegularExpressions.Regex -ArgumentList $patternToExtract
+    $maxVersion = [version] "0.0.0"
+
+    try {
+        $moduleFolders = Get-ChildItem -Directory -Path $($env:SystemDrive + "\Modules") | Where-Object { $regexToMatch.IsMatch($_.Name) }
+        foreach ($moduleFolder in $moduleFolders) {
+            $moduleVersion = [version] $($regexToExtract.Match($moduleFolder.Name).Groups[0].Value)
+            if($moduleVersion -gt $maxVersion) {
+                $modulePath = [System.IO.Path]::Combine($moduleFolder.FullName,"AzureRM\$moduleVersion\AzureRM.psm1")
+
+                if(Test-Path -LiteralPath $modulePath -PathType Leaf) {
+                    $maxVersion = $moduleVersion
+                    $resultFolder = $moduleFolder.FullName
+                } else {
+                    Write-Verbose "A folder matching the module folder pattern was found at $($moduleFolder.FullName) but didn't contain a valid module file"
+                }
+            }
+        }
+    }
+    catch {
+        Write-Verbose "Attempting to find the Latest Module Folder failed with the error: $($_.Exception.Message)"
+        $resultFolder = ""
+    }
+    Write-Verbose "Latest module folder detected: $resultFolder"
+    return $resultFolder
+}
+
+function Import-AzureModule {
     [CmdletBinding()]
     param([switch]$PreferAzureRM)
 
     Trace-VstsEnteringInvocation $MyInvocation
     try {
-        Write-Verbose "Env:PSModulePath: '$env:PSMODULEPATH'"
+        #try to get latest azurerm_ module installed on agent
+        $hostedAgentAzureRmModulePath = Get-LatestModule -patternToMatch "^azurerm_[0-9]+\.[0-9]+\.[0-9]+$" -patternToExtract "[0-9]+\.[0-9]+\.[0-9]+$";
+        $env:PSModulePath = $env:PSModulePath.TrimStart(';');
+        Write-Verbose "Env:PSModulePath: '$env:PSMODULEPATH'";
         if ($PreferAzureRM) {
             if (!(Import-FromModulePath -Classic:$false) -and
                 !(Import-FromSdkPath -Classic:$false) -and
@@ -128,3 +165,4 @@ function Import-FromSdkPath {
         Trace-VstsLeavingInvocation $MyInvocation
     }
 }
+

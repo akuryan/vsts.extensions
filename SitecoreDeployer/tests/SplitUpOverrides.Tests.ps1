e7d30f64b2498ebe899f3f3c2d67500018485066
Anton Kuryan <anton.kuryan@colours.nl>
2019-05-15 17:56:24 +0300

    [no ci] Added almost real life test case

--- SitecoreDeployer/tests/SplitUpOverrides.Tests.ps1
+++ SitecoreDeployer/tests/SplitUpOverrides.Tests.ps1
@@ -1,8 +1,10 @@
 # this will test a function SplitUpOverrides in functions-module-v1.psm1
 Write-Host "Iteration 1";
-$testString = '-templateLinkBase https://testblob.blob.core.windows.net/testcontainer/20/test/ -templateLinkAccessToken ?st=2018-01-31T08%3A26%3A47Z&se=2018-02-01T08%3A26%3A47Z&sp=r&sv=2017-03-28&sr=b&sig=7asodasdayodolasKernxyadYfcqlGZxQZU%3D';
+$testString = '-templateLinkBase https://testblob.blob.core.windows.net/testcontainer/20/test/ -templateLinkAccessToken ?st=2018-01-31T08%3A26%3A47Z&se=2018-02-01T08%3A26%3A47Z&sp=r&sv=2017-03-28&sr=b&sig=7asodasdayodolasKernxyadYfcqlGZxQZU%3D -siMsDeployPackageUrl "https://testblob.blob.core.windows.net/sitecore/version/si.scwdp.zip" -cmMsDeployPackageUrl "https://testblob.blob.core.windows.net/sitecore/version/scaled/cm.scwdp.zip"';
 $testHashTable1 = SplitUpOverrides -inputString $testString;
 CompareValuesAndExitIfFail -value1 $testHashTable1["templateLinkBase"] -value2 "https://testblob.blob.core.windows.net/testcontainer/20/test/";
+CompareValuesAndExitIfFail -value1 $testHashTable1["siMsDeployPackageUrl"] -value2 "https://testblob.blob.core.windows.net/sitecore/version/si.scwdp.zip";
+CompareValuesAndExitIfFail -value1 $testHashTable1["cmMsDeployPackageUrl"] -value2 "https://testblob.blob.core.windows.net/sitecore/version/scaled/cm.scwdp.zip";
 
 Write-Host "Iteration 2";
 $testString = '-test 123 -test2 1234 -test3 "123 123"'
